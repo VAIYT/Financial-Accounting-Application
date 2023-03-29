ffd62305d0c35bc73731e19c2f9499ba97e0f36e
dehbka <denis_r@chillicode.ru>
2023-03-30 01:54:21 +0300

    Добавил форму трат

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,17 +1,41 @@
 import "../App.css";
-import { Button, Loading } from "@nextui-org/react";
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Grid,
+  Input,
+  Loading,
+  Row,
+} from "@nextui-org/react";
 import { Default } from "../layouts/Default";
 
 export const App = () => {
   return (
     <Default>
-      <Button>
-        {true ? (
-          "SEND"
-        ) : (
-          <Loading type="points-opacity" color="currentColor" size="sm" />
-        )}
-      </Button>
+      <Container gap={1}>
+        <Card css={{ shadow: "$md" }}>
+          <Card.Header>Добавить трату</Card.Header>
+          <Card.Body>
+            <Row>
+              <Col span={2}>
+                <Input width="100%" placeholder="Next UI" />
+              </Col>
+            </Row>
+            <Row>
+              <Col span={2}>
+                <Input width="100%" placeholder="Next UI" />
+              </Col>
+            </Row>
+            <Row>
+              <Col span={2} justify="center">
+                <Button>Добавить</Button>
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      </Container>
     </Default>
   );
 };
