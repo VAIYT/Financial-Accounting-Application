efb72ab3472b2a29a9d1a2122fba4e410060ca6e
VAIYT <evgenijstesenko68583@gmail.com>
2023-03-30 00:37:56 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button, Loading } from '@nextui-org/react';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>{true ? 'SEND': <Loading type="points-opacity" color="currentColor" size="sm" />}</Button>
+    </div>
+  );
+}
+
+export default App;
